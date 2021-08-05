37bfd7b02e80b389cf3e064732a3ae94f9f25fd3
Sora <petzuo@yahoo.co.jp>
2021-08-05 22:00:14 +0800

    优化启动流程

--- pages/home/home.vue
+++ pages/home/home.vue
@@ -1,15 +1,21 @@
 <template>
 	<view>
-		<u-navbar :is-back="false" title="" :show-title="false" :background="{backgroundColor: $cfg.background_color}" :border-bottom="false">
+		<u-navbar :is-back="false" title="" :show-title="false" :background="{backgroundColor: $cfg.background_color}" :border-bottom="false" v-show="current != -1">
 			<text class="nav-item">HFUTGo</text>
 			<u-loading mode="circle" class="nav-item" :color="$cfg.theme_color" size="40" :show="showLoading"></u-loading>
 		</u-navbar>
 		<view class="body">
-			<menu-page v-show="current == 0" v-model="current"/>
-			<today v-show="current == 1" v-model="showLoading" />
-			<my v-show="current == 2" />
+			<view v-if="current == -1">
+				<u-top-tips ref="uTips"></u-top-tips>
+				<view class="tip">
+					<u-loading size="100" :color="$cfg.theme_color"></u-loading>
+				</view>
+			</view>
+			<menu-page v-if="current != -1" v-show="current == 0" v-model="current"/>
+			<today v-if="current != -1" v-show="current == 1" v-model="showLoading" />
+			<my v-if="current != -1" v-show="current == 2" />
 		</view>
-		<u-tabbar v-model="current" :list="list" :mid-button="true" active-color="#4da0e0" :border-top="true"></u-tabbar>
+		<u-tabbar v-model="current" :list="list" :mid-button="true" active-color="#4da0e0" :border-top="true" v-show="current != -1"></u-tabbar>
 		<s-popup :title="`v${updateLog_[0].version} 更新日志`" v-model="showUpdate" mode="center">
 			<rich-text :nodes="updateLog_[0].log"></rich-text>
 		</s-popup>
@@ -29,7 +35,7 @@
 		},
 		data() {
 			return {
-				current: 1,
+				current: -1,
 				list: [
 					{
 						iconPath: "grid",
@@ -63,7 +69,32 @@
 			    wx.hideHomeButton();  
 			}  
 			// #endif
-			this.loadUpdate()
+			if(!uni.getStorageSync('userInfo')){
+				uni.reLaunch({
+					url: '/pages/user/login'
+				})
+				return
+			}
+			this.$user.initialize().then(data => {
+				this.current = 1
+				this.loadUpdate()
+			}).catch(err => {
+				if(err == 'password_error'){
+					this.$refs.uTips.show({
+						title: '密码错误',
+						type: 'error'
+					})
+					setTimeout(function(){
+						uni.reLaunch({
+							url: '/pages/user/login'
+						})
+					}, 2000)
+				}else{
+					this.current = 1
+					this.loadUpdate()
+				}
+				console.log(err)
+			})
 		},
 		methods: {
 			loadUpdate(){
@@ -87,5 +118,11 @@
 	}
 	.nav-item{
 		margin-left: 30rpx;
+	}
+	.tip{
+		position: absolute;
+		top: 45%;
+		left: 50%;
+		transform: translate(-50%, -50%);
 	}
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,6 @@
 <template>
 	<view>
-		<u-top-tips ref="uTips"></u-top-tips>
-		<view class="tip">
-			<u-loading size="100" :color="$cfg.theme_color"></u-loading>
-		</view>
+		
 	</view>
 </template>
 
@@ -13,45 +10,10 @@
 			return {
 				
 			};
-		},
-		onLoad() {
-			if(!uni.getStorageSync('userInfo')){
-				uni.reLaunch({
-					url: '/pages/user/login'
-				})
-				return
-			}
-			this.$user.initialize().then(data => {
-				uni.reLaunch({
-					url: '/pages/home/home'
-				})
-			}).catch(err => {
-				if(err == 'password_error'){
-					this.$refs.uTips.show({
-						title: '密码错误',
-						type: 'error'
-					})
-					setTimeout(function(){
-						uni.reLaunch({
-							url: '/pages/user/login'
-						})
-					}, 2000)
-				}else{
-					uni.reLaunch({
-						url: '/pages/home/home'
-					})
-				}
-				console.log(err)
-			})
 		}
 	}
 </script>
 
 <style lang="scss">
-	.tip{
-		position: absolute;
-		top: 45%;
-		left: 50%;
-		transform: translate(-50%, -50%);
-	}
+	
 </style>
